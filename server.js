ce8221d121f52cfcd10759bc21a63dafd9601579
backup96 <109244276+backup96@users.noreply.github.com>
2024-11-16 15:10:01 -0500

    Solucionando

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://front-36ehwz5h1-backup96s-projects.vercel.app", // Dominios permitidos
+    origin: "https://front-betk1gzh3-backup96s-projects.vercel.app", // Dominios permitidos
     methods: "GET,POST,PUT,DELETE", // Métodos HTTP permitidos
     credentials: true, // Si necesitas enviar cookies o headers de autenticación
   })
